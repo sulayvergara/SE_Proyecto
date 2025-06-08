3e14441f82aea07340d9e43a57d0bef2da85bce3
Ivan Hernandez <ihernandezq@est.ups.edu.ec>
2025-06-08 11:58:04 -0500

    Router/cliente

--- app/routers/cliente.py
+++ app/routers/cliente.py
@@ -7,18 +7,18 @@ from typing import List
 
 router = APIRouter()
 
-@router.post("/", response_model=ClienteCreate, tags=["Clientes"])
+@router.post("/clientes/", response_model=ClienteCreate, tags=["Clientes"])
 async def create_cliente(cliente: ClienteCreate, db: AsyncSession = Depends(get_async_session)):
     existente = await crud_cliente.get_cliente(db, cliente.documento_identidad)
     if existente:
         raise HTTPException(status_code=400, detail="El cliente ya existe")
     return await crud_cliente.create_cliente(db, cliente)
 
-@router.get("/", response_model=List[Cliente], tags=["Clientes"])
+@router.get("/clientes/", response_model=List[Cliente], tags=["Clientes"])
 async def listar_clientes(db: AsyncSession = Depends(get_async_session)):
-    return await crud_cliente.create_cliente(db)
+    return await crud_cliente.list_clientes(db)
 
-@router.get("/{cliente_id}", response_model=Cliente, tags=["Clientes"])
+@router.get("/clientes/{cliente_id}", response_model=Cliente, tags=["Clientes"])
 async def create_cliente(cliente_id: int, db: AsyncSession = Depends(get_async_session)):
     cliente = await crud_cliente.get_cliente(db, cliente_id)
     if not cliente:
